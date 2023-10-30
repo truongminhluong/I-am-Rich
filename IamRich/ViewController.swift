670b847448520ea22bb2cc8410892b387fc101e4
truongminhluong <truongminhluong2003@gmail.com>
2023-10-30 22:41:20 +0700

    abc

--- IamRich/ViewController.swift
+++ IamRich/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isHidden = true
+        labelView.isHidden = true
+        hienImageLabel()
+    }
+    
+    func hienImageLabel() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.imageView.isHidden = false
+            self.labelView.isHidden = false
+        }
     }
 
 
